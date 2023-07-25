61d4c5405fbf98bd6ee6bf48280ed2b13982c452
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-25 23:26:16 +0300

    feat(App.js): setup the App.js include apollo client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Home from './Pages/Home';
+import "./App.css";
+import Home from "./Pages/Home";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https:/graphql-weather-api.herokuapp.com/"
+
+  })
+
   return (
-    <div className='App'>
-    <Home/>    
-    </div>
-  
-    );
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
+  );
 }
 
 export default App;
