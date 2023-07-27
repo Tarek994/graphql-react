bc2fa34dd5afdf2792c2fe60fe5e72a2f08d7a68
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-28 00:54:39 +0300

    feat(Home): display the weather data from API

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
-import { useLazyQuery } from "@apollo/client"
-import { GET_WEATHER_QUERY } from "../graphql/Queries"
-
+import { useLazyQuery } from "@apollo/client";
+import { useState } from "react";
+import { GET_WEATHER_QUERY } from "../graphql/Queries";
 
 function Home() {
   const [citySearched, setCitySearched] = useState("");
@@ -17,9 +17,9 @@ function Home() {
     console.log("Data", data);
   }
 
-    return (
-        <div className="home">
-            <h1> Search For Weather</h1>
+  return (
+    <div className="home">
+      <h1> Search For Weather</h1>
       <input
         type="text"
         placeholder="City name ..."
@@ -28,7 +28,23 @@ function Home() {
         }}
       />
       <button onClick={() => getWeather()}>Search </button>
-    )
+      <div className="weather">
+        {data && (
+          <>
+            <h1> {data.getCityByName.name} </h1>
+            <h1>
+              {" "}
+              Temperature: {data.getCityByName.weather.temperature.actual}
+            </h1>
+            <h1>
+              Description: {data.getCityByName.weather.summary.description}
+            </h1>
+            <h1>Wind Speed: {data.getCityByName.weather.wind.speed}</h1>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
