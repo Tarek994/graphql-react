f8a6142d5d0bc4c955aeb6e155b6c3c329bd5645
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-27 23:50:38 +0300

    feat(Home.js): import useLazyQuery hook to home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,13 @@
+import { useLazyQuery } from "@apollo/client"
+import { GET_WEATHER_QUERY } from "../graphql/Queries"
+
+
 function Home() {
     return (
         <div className="home">
-                    <h1> Search For Weather</h1>
-        <input type="text" placeholder="City name ..." />
-        <button>Search </button>
+            <h1> Search For Weather</h1>
+            <input type="text" placeholder="City name ..." />
+            <button>Search </button>
         </div>
     )
 }
